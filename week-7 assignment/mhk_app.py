c14db2245c7300eef0d300f7766214def4b1c599
Min Han Kyaw <minnhankyaw056@gmail.com>
2026-05-15 00:19:59 +0630

    Added flash messages and update style.css

--- week-7 assignment/mhk_app.py
+++ week-7 assignment/mhk_app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import mhk_product_service as service
 
 app = Flask(__name__)
+app.secret_key = "2006"
 
 @app.route("/")
 def index():
@@ -17,9 +18,12 @@ def create_product():
     if request.method == "POST":
         name = request.form.get("name")
         price = request.form.get("price")
-        if not name or not price or float(price) < 0:
-            return render_template("product.html", error="Invalid input!")
+        if not name or name.isdigit():
+            flash("Product name must not be number or empty!", "danger")
+            flash("Invalid input!","danger")
+            return render_template("product.html")
         service.add_product(name, price)
+        flash("Product added successfully!", "success")
         return redirect(url_for('list_products'))
     return render_template("product.html")
 
@@ -29,14 +33,15 @@ def edit_product(index):
         name = request.form.get("name")
         price = request.form.get("price")
         service.update_product(index, name, price)
+        flash("Product updated successfully!","success")
         return redirect(url_for('list_products'))
-    
     current = service.get_product_by_index(index)
     return render_template("product_edit.html", current=current, index=index)
 
 @app.route("/product/delete/<int:index>", methods=["POST"])
 def delete_product(index):
     service.delete_product(index)
+    flash("Product deleted successfully!","success")
     return redirect(url_for('list_products'))
 
 if __name__ == "__main__":
